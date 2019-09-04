ac1a962aaff829c4be41cca057d93ab5c14e759d
Niharika <niharikasundarigari@gmail.com>
2019-09-04 13:28:55 -0400

    initial

---
+++ animation/ex5.js
@@ -0,0 +1,14 @@
+ $(document).ready(function() {
+	 	
+		 	$('#btnFadeOut').click(function() {
+		 		$('#box').fadeOut(3000);
+		 	});
+		
+			 $('#btnFadeIn').click(function() {
+			 	$('#box').fadeIn(300);
+				
+				}); 
+			});
+
+
+		  
